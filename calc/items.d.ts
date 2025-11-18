cdc53413e817f70326ace50506558fd0ee0c5d5b
theAstrogoth <theastrogoth@gmail.com>
2025-11-18 09:33:21 -0600

    publish no. 7

--- calc/items.d.ts
+++ calc/items.d.ts
@@ -4,7 +4,7 @@ export declare const SEED_BOOSTED_STAT: {
 };
 export declare function getItemBoostType(item: string | undefined): "Electric" | "Psychic" | "Normal" | "Fighting" | "Flying" | "Poison" | "Ground" | "Rock" | "Bug" | "Ghost" | "Steel" | "Fire" | "Water" | "Grass" | "Ice" | "Dragon" | "Dark" | "Fairy" | undefined;
 export declare function getBerryResistType(berry: string | undefined): "Electric" | "Psychic" | "Normal" | "Fighting" | "Flying" | "Poison" | "Ground" | "Rock" | "Bug" | "Ghost" | "Steel" | "Fire" | "Water" | "Grass" | "Ice" | "Dragon" | "Dark" | "Fairy" | undefined;
-export declare function getFlingPower(item?: string): 10 | 0 | 30 | 100 | 40 | 60 | 50 | 85 | 20 | 130 | 110 | 95 | 90 | 80 | 70;
+export declare function getFlingPower(item?: string): 10 | 0 | 100 | 50 | 30 | 60 | 85 | 110 | 20 | 90 | 40 | 80 | 130 | 95 | 70;
 export declare function getNaturalGift(gen: Generation, item: string): {
     t: TypeName;
     p: number;
